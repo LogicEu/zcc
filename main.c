67c0e6b1a5f1492e27479d84f43e210ef2987518
LogicEu <eugenioarteagaa@gmail.com>
2022-09-29 11:51:56 -0300

    After long strugle, managed to succeed in #include function and ptu_merge function. Now need to work on #define so we don't do infinite recursion inclusions.

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ISNONE(c) (((c) == ' ') || ((c) == '\n') || ((c) == '\t'))
-#define ISSTR(c) (((c) == '\'') || ((c) == '"'))
-#define ISBETWEEN(c, a, b) (((c) >= (a)) && ((c) <= (b)))
-#define ISNUM(c) ISBETWEEN(c, '0', '9')
-#define ISID(c) (ISBETWEEN(c, 'A', 'Z') || ISBETWEEN(c, 'a', 'z') || ((c) == '_'))
-#define ISSYMBOL(c) (ISBETWEEN(c, 33, 126) && !ISNUM(c) && !ISID(c))
+#define chrspace(c) (((c) == ' ') || ((c) == '\n') || ((c) == '\t'))
+#define chrstr(c) (((c) == '\'') || ((c) == '"'))
+#define chrbetween(c, a, b) (((c) >= (a)) && ((c) <= (b)))
+#define chrdigit(c) chrbetween(c, '0', '9')
+#define chralpha(c) (chrbetween(c, 'A', 'Z') || chrbetween(c, 'a', 'z') || ((c) == '_'))
 
 typedef struct range_t {
     size_t start, end;
@@ -21,14 +20,6 @@ typedef struct ptu_t {
     array_t tokens;
 } ptu_t;
 
-static inline size_t eatblock(const char* str, const char ch, const size_t end, size_t i)
-{
-    while (i + 1 < end && str[i + 1] != ch) { 
-        i += (str[i + 1] == '\\') + 1;
-    } 
-    return i + 1;
-}
-
 static inline void printrng(const char* str, const range_t range)
 {
     for (size_t i = range.start; i < range.end; ++i) {
@@ -36,33 +27,34 @@ static inline void printrng(const char* str, const range_t range)
     }
 }
 
-static void ptu_print_lines(const ptu_t* ptu)
+static range_t ptu_tokens_at(const range_t* toks, const size_t count, range_t range)
 {
-    const range_t* r = ptu->lines.data;
-    const size_t count = ptu->lines.size;
-    for (size_t i = 0; i < count; ++i) {
-        putchar('\'');
-        printrng(ptu->text.data, r[i]);
-        putchar('\'');
-        putchar('\n');
-    }
+    size_t i;
+    for (i = 0; i < count && toks[i].start < range.start; ++i);
+    for (range.start = i; i < count && toks[i].end <= range.end; ++i);
+    range.end = i;
+    return range;
 }
 
 static void ptu_print_tokens(const ptu_t* ptu)
 {
-    size_t i, j;
-    const size_t count = ptu->tokens.size;
-    const range_t* r = ptu->tokens.data, *l = ptu->lines.data;
-    for (i = 0, j = 0; i < count; ++i) {
-        while (r[i].start >= l[j].end) {
-            ++j;
+    const char* s = ptu->text.data;
+    const size_t linecount = ptu->lines.size, tokencount = ptu->tokens.size;
+    const range_t* lines = ptu->lines.data, *tokens = ptu->tokens.data;
+    for (size_t i = 0; i < linecount; ++i) {
+        const range_t toks = ptu_tokens_at(tokens, tokencount, lines[i]);
+        for (size_t j = toks.start; j < toks.end; ++j) {
+            putchar('\'');
+            printrng(s, tokens[j]);
+            putchar('\'');
+            putchar(' ');
         }
-
+        putchar('\n');
+        
+        /*putchar('\'');
+        printrng(s, lines[i]);
         putchar('\'');
-        printrng(ptu->text.data, r[i]); 
-        putchar('\'');
-        putchar(r[i].end >= l[j].end ? '\n' : ' ');
-
+        putchar('\n');*/
     }
 }
 
@@ -73,28 +65,15 @@ static void ptu_free(ptu_t* ptu)
     array_free(&ptu->tokens);
 }
 
-static void ptu_splice_lines(ptu_t* ptu)
+static inline size_t eatblock(const char* str, const char ch, const size_t end, size_t i)
 {
-    size_t i;
-    range_t r = {0, 0};
-    for (i = 0; ptu->text.data[i]; ++i) {
-        if (ptu->text.data[i] == '\n') {
-            if (i > 0 && ptu->text.data[i - 1] == '\\') {
-                string_remove_index(&ptu->text, i--);
-                string_remove_index(&ptu->text, i--);
-            } else {
-                r.end = i;
-                array_push(&ptu->lines, &r);
-                r.start = i + 1;
-            }
-        }
-    }
-    
-    r.end = i;
-    array_push(&ptu->lines, &r);
+    while (i + 1 < end && str[i + 1] != ch) { 
+        i += (str[i + 1] == '\\') + 1;
+    } 
+    return i + 1;
 }
 
-static size_t eatsymbol(const char* str, const size_t i)
+static inline size_t eatsymbol(const char* str, const size_t i)
 {
     size_t j = 1;
     switch(str[i]) {
@@ -125,6 +104,30 @@ static size_t eatsymbol(const char* str, const size_t i)
     return i + j;
 }
 
+static void ptu_splice_lines(ptu_t* ptu)
+{
+    size_t i;
+    range_t r = {0, 0};
+    for (i = 0; ptu->text.data[i]; ++i) {
+        if (ptu->text.data[i] == '\n') {
+            if (i > 0 && ptu->text.data[i - 1] == '\\') {
+                string_remove_index(&ptu->text, i--);
+                string_remove_index(&ptu->text, i--);
+            } else {
+                r.end = i;
+                array_push(&ptu->lines, &r);
+                r.start = i + 1;
+            }
+        }
+    }
+   
+    if (i > r.start) { 
+        r.end = i;
+        array_push(&ptu->lines, &r);
+    }
+}
+
+
 static void ptu_splice_tokens(ptu_t* ptu)
 {
     range_t r = {0, 0};
@@ -133,20 +136,20 @@ static void ptu_splice_tokens(ptu_t* ptu)
         
         r.start = i;
         
-        if (ISSTR(s[i])) {
+        if (chrstr(s[i])) {
             i = eatblock(s, s[i], ptu->text.size, i) + 1;
         }
-        else if (ISNUM(s[i]) || (s[i] == '.' && ISNUM(s[i + 1]))) {
-            while (s[i] && (ISNUM(s[i]) || ISID(s[i]) || s[i] == '.')) {
+        else if (chrdigit(s[i]) || (s[i] == '.' && chrdigit(s[i + 1]))) {
+            while (s[i] && (chrdigit(s[i]) || chralpha(s[i]) || s[i] == '.')) {
                 ++i;
             }
         }
-        else if (ISID(s[i])) {
-            while (s[i] && (ISID(s[i]) || ISNUM(s[i]))) {
+        else if (chralpha(s[i])) {
+            while (s[i] && (chralpha(s[i]) || chrdigit(s[i]))) {
                 ++i;
             }
         }
-        else if (!ISNONE(s[i])) {
+        else if (!chrspace(s[i])) {
             i = eatsymbol(s, i);
         }
         else continue;
@@ -156,11 +159,8 @@ static void ptu_splice_tokens(ptu_t* ptu)
     }
 }
 
-/* needs more error checking for block closures */
 static void ptu_preprocess_text(ptu_t* ptu)
 {
-    ptu_splice_lines(ptu);
-    
     char* s = ptu->text.data, *t;
     size_t i, j, k, d, n;
     range_t* r = ptu->lines.data;
@@ -206,10 +206,7 @@ static void ptu_preprocess_text(ptu_t* ptu)
             r[++j].start -= d;
             r[j].end -= d;
         }
-
     }
-
-    ptu_splice_tokens(ptu);
 }
 
 static ptu_t ptu_read(const char* filename)
@@ -222,7 +219,6 @@ static ptu_t ptu_read(const char* filename)
     
     FILE* file = fopen(filename, "rb");
     if (!file) {
-        printf("Could not open file '%s'.\n", filename);
         return ptu;
     }
     
@@ -233,19 +229,179 @@ static ptu_t ptu_read(const char* filename)
     fread(ptu.text.data, 1, size, file);
     fclose(file);
     ptu.text.data[size] = 0;
-    ptu.text.size = size;
+    ptu.text.size = size; 
+    ptu_splice_lines(&ptu);
     ptu_preprocess_text(&ptu);
+    ptu_splice_tokens(&ptu);
     return ptu;
+}
+
+static void rangesoffset(const array_t* ranges, const size_t off, const size_t from)
+{
+    range_t* n = ranges->data;
+    for (size_t i = from; i < ranges->size; ++i) {
+        n[i].start += off;
+        n[i].end += off;
+    }
+}
+
+static void ptu_merge(ptu_t* dest, ptu_t* src, const range_t toks, const size_t index)
+{
+    const range_t* lines = dest->lines.data;
+    const range_t line = lines[index];
+    const size_t linelen = line.end - line.start + 1;
+
+    rangesoffset(&src->lines, line.start, 0);
+    rangesoffset(&src->tokens, line.start, 0);
+   
+    string_remove_range(&dest->text, line.start, line.end + 1);
+    array_remove(&dest->lines, index);
+    array_remove_block(&dest->tokens, toks.start, toks.end);
+   
+    rangesoffset(&dest->lines, src->text.size - linelen, index);
+    rangesoffset(&dest->tokens, src->text.size - linelen, toks.start); 
+     
+    string_push_at(&dest->text, src->text.data, line.start); 
+    array_push_block_at(&dest->lines, src->lines.data, src->lines.size, index);
+    array_push_block_at(&dest->tokens, src->tokens.data, src->tokens.size, toks.start);
+   
+    ptu_free(src);
+}
+
+static void ptu_include(ptu_t* ptu, const array_t* includes,
+                        const range_t line, size_t* index)
+{    
+    ptu_t p;
+    char filename[0xfff];
+    const char* s = ptu->text.data; 
+    const range_t* toks = ptu->tokens.data;
+    const size_t incopt = toks[line.start + 2].start;
+    const size_t cmp = (s[incopt] == '"');
+    const size_t start = line.start + 2 + !cmp;
+    const size_t end = cmp ? start : line.end - 1;
+    const range_t r = {toks[start].start + cmp, toks[end].end - 1}; 
+
+    const size_t len = r.end - r.start;
+    memcpy(filename, s + r.start, len);
+    filename[len] = 0;
+    
+    if (s[incopt] == '"') {
+        p = ptu_read(filename);
+    }
+    else if (s[incopt] == '<') {
+        char dir[0xfff];
+        const size_t count = includes->size;
+        const char** idirs = includes->data;;
+        for (size_t i = 0, dirlen; i < count; ++i) {
+            dirlen = strlen(idirs[i]);
+            memcpy(dir, idirs[i], dirlen);
+            if (dir[dirlen - 1] != '/') {
+                dir[dirlen++] = '/';
+            }
+            memcpy(dir + dirlen, filename, len + 1);
+            p = ptu_read(dir);
+            if (p.text.size) {
+                break;
+            }
+        }
+    }
+
+    
+    if (!p.text.size) {
+        printf("Could not open header file '%s'.\n", filename);
+        return;
+    }
+    
+    ptu_merge(ptu, &p, line, (*index)--);
+}
+
+/*static void ptu_define(ptu_t* ptu, map_t* defines, const range_t line)
+{
+    const char* s = ptu->text.data;
+    const range_t* toks = ptu->tokens.data;
+    for (size_t i = line.start; i < line.end; ++i) {
+        printrng(s, toks[i]);
+        putchar(' ');
+    }
+    putchar('\n');
+}
+
+static void ptu_ifdef(ptu_t* ptu, const map_t* defines, const range_t line)
+{
+    const char* s = ptu->text.data;
+    const range_t* toks = ptu->tokens.data;
+    for (size_t i = line.start; i < line.end; ++i) {
+        printrng(s, toks[i]);
+        putchar(' ');
+    }
+    putchar('\n');
+}
+
+static void ptu_undef(ptu_t* ptu, map_t* defines, const range_t line)
+{
+    
+}*/
+
+static void ptu_preprocess_macros(ptu_t* ptu, const array_t* includes)
+{
+    static const char* inc = "include", *def = "define", *ifdef = "if", *undef = "undef";
+    const size_t inclen = strlen(inc), deflen = strlen(def);
+    const size_t ifdeflen = strlen(ifdef), undeflen = strlen(undef);
+
+    map_t defines = map_create(sizeof(string_t), sizeof(string_t));
+
+    size_t i, j;
+    for (i = 0; i < ptu->lines.size; ++i) {
+        const range_t* lines = ptu->lines.data;
+        if (lines[i].start == lines[i].end) {
+            continue;
+        }
+
+        const range_t* tokens = ptu->tokens.data;
+        const range_t linetoks = ptu_tokens_at(tokens, ptu->tokens.size, lines[i]);
+        if (linetoks.start == linetoks.end) {
+            continue;
+        }
+
+        j = tokens[linetoks.start].start;
+        if (ptu->text.data[j] == '#') {
+            j = tokens[linetoks.start + 1].start;
+            if (!memcmp(ptu->text.data + j, inc, inclen)) {
+                ptu_include(ptu, includes, linetoks, &i);
+            }
+            /*else if (!memcmp(s + j, def, deflen)) {
+                ptu_define(ptu, &defines, linetoks);
+            }
+            else if (!memcmp(s + j, ifdef, ifdeflen)) {
+                ptu_ifdef(ptu, &defines, linetoks);
+            }
+            else if (!memcmp(s + j, undef, undeflen)) {
+                ptu_undef(ptu, &defines, linetoks);
+            }*/
+        } 
+    }
+}
+
+static array_t stdincludes(void)
+{
+    static const char* stddirs[] = {".", "/usr/include/", "/usr/local/include/",
+        ("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/"
+        "Developer/SDKs/MacOSX.sdk/usr/include")
+    };
+
+    array_t includes = array_create(sizeof(char*));
+    array_push_block(&includes, stddirs, sizeof(stddirs) / sizeof(stddirs[0]));
+    return includes;
 }
 
 int main(const int argc, const char** argv)
 {
     int status = EXIT_SUCCESS;
     array_t infiles = array_create(sizeof(char*));
-    array_t includes = array_create(sizeof(char*));
+    array_t includes = stdincludes();
 
     for (int i = 1; i < argc; ++i) {
-        if (argv[i][0] == 45) {
+        if (argv[i][0] == '-') {
             if (argv[i][1] == 'I') {
                 const char* ptr = argv[i] + 2;
                 array_push(&includes, &ptr);
@@ -265,10 +421,11 @@ int main(const int argc, const char** argv)
     for (size_t i = 0; i < filecount; ++i) {
         ptu_t ptu = ptu_read(filepaths[i]);
         if (ptu.text.size) {
-            ptu_print_lines(&ptu);
+            ptu_preprocess_macros(&ptu, &includes);
             ptu_print_tokens(&ptu);
+            ptu_free(&ptu);
         }
-        ptu_free(&ptu);
+        else printf("Could not open file '%s'.\n", filepaths[i]);
     }
 
 exit:
